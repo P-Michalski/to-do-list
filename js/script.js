d349670909b5a77f74f738724fc3f6cdc3251ca7
Piotr Michalski <pipi6620@gmail.com>
2023-04-27 16:22:07 +0200

    Added ability to remove and toggle tasks done

--- js/script.js
+++ js/script.js
@@ -8,17 +8,56 @@
 
     const render = () => {
         let htmlString = "";
-
         for (const task of tasks) {
             htmlString += `
-                <li>
-                    ${task.content}
+                <li class="taskSection__listItem">
+                    <button class="taskSection__button js-doneButton">${task.done ? "✔" : ""}</button>
+                    <span${task.done ? " class=\"taskSection__listItemText--done\"" : ""}>${task.content}</span>
+                    <button class="taskSection__button js-removeButton">🗑</button>
                 </li>
             `;
         };
 
         document.querySelector(".js-taskList").innerHTML = htmlString;
+        bindEvents();
     };
+
+    const addNewTask = (newTaskContent) => {
+        const newTask = document.querySelector(".js-newTask");
+        tasks.push({
+            content: newTaskContent,
+            done: false,
+        });
+        newTask.value = "";
+        newTask.focus();
+        render();
+    };
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    };
+
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
+        render();
+    };
+
+    const bindEvents = () => {
+        const removeButtons = document.querySelectorAll(".js-removeButton");
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
+
+        const doneButtons = document.querySelectorAll(".js-doneButton");
+        doneButtons.forEach((doneButton, index) => {
+            doneButton.addEventListener("click", () => {
+                toggleTaskDone(index);
+            });
+        });
+    }
 
     const init = () => {
         render();
@@ -30,11 +69,8 @@
             if (newTaskContent === "") {
                 return;
             }
-            tasks.push({
-                content: newTaskContent,
-                done: false,
-            });
-            render();
+
+            addNewTask(newTaskContent);
         });
     };
 
